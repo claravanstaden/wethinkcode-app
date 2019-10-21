85a1f0cecc4540342ba6804cf53991c43ac58531
claravanstaden <clara@overhq.com>
2019-10-21 06:49:29 +0200

    Add create functionality.

--- js/app.js
+++ js/app.js
@@ -2,12 +2,45 @@ new Vue({
     el: '#app',
     data () {
         return {
-            users: null
+            users: null,
+            email: null,
+            password: null,
+            name: null
         }
     },
     mounted () {
         axios
             .get('http://localhost:8000/api/users')
-            .then(response => (this.users = response.data))
+            .then((response) => {
+                this.users = response.data
+            }).catch((error) => {
+                alert("Whoops, API not running!")
+        })
+    },
+    methods: {
+        createUser(e) {
+            e.preventDefault();
+            let currentObj = this;
+            axios.post('http://localhost:8000/api/users', {
+                name: currentObj.name,
+                email: currentObj.email,
+                password: currentObj.password
+            })
+                .then(function (response) {
+                    currentObj.name = null;
+                    currentObj.email = null;
+                    currentObj.password = null;
+                    axios
+                        .get('http://localhost:8000/api/users')
+                        .then((response) => {
+                            currentObj.users = response.data
+                        }).catch((error) => {
+                        alert("Whoops, API not running!")
+                    })
+                })
+                .catch(function (error) {
+                    alert("Whoops, student could not be added!")
+                });
+        }
     }
 });
