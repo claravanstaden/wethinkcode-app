4ed60f8e60c6cbf0b8ad21874de94a497bb63f8f
claravanstaden <clara@overhq.com>
2019-10-21 06:56:51 +0200

    Refactor.

--- js/app.js
+++ js/app.js
@@ -1,46 +1,46 @@
-new Vue({
-    el: '#app',
-    data () {
-        return {
-            users: null,
-            email: null,
-            password: null,
-            name: null
-        }
-    },
-    mounted () {
+(function () {
+    function getUsers() {
         axios
             .get('http://localhost:8000/api/users')
             .then((response) => {
                 this.users = response.data
             }).catch((error) => {
-                alert("Whoops, API not running!")
+            alert("Whoops, API not running!")
         })
-    },
-    methods: {
-        createUser(e) {
-            e.preventDefault();
-            let currentObj = this;
-            axios.post('http://localhost:8000/api/users', {
-                name: currentObj.name,
-                email: currentObj.email,
-                password: currentObj.password
-            })
-                .then(function (response) {
-                    currentObj.name = null;
-                    currentObj.email = null;
-                    currentObj.password = null;
-                    axios
-                        .get('http://localhost:8000/api/users')
-                        .then((response) => {
-                            currentObj.users = response.data
-                        }).catch((error) => {
-                        alert("Whoops, API not running!")
-                    })
-                })
-                .catch(function (error) {
-                    alert("Whoops, student could not be added!")
-                });
-        }
     }
-});
+
+    new Vue({
+        el: '#app',
+        data() {
+            return {
+                users: null,
+                email: null,
+                password: null,
+                name: null
+            }
+        },
+        mounted() {
+            getUsers.call(this);
+        },
+        methods: {
+            createUser(e) {
+                e.preventDefault();
+                let currentObj = this;
+                axios.post('http://localhost:8000/api/users', {
+                    name: currentObj.name,
+                    email: currentObj.email,
+                    password: currentObj.password
+                })
+                    .then(function (response) {
+                        currentObj.name = null;
+                        currentObj.email = null;
+                        currentObj.password = null;
+                        getUsers(this);
+                    })
+                    .catch(function (error) {
+                        alert("Whoops, student could not be added!")
+                    });
+            }
+        }
+    });
+})();
